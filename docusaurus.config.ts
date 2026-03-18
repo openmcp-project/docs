a420972669fb970f902868f4a8382c6a6c51d5b0
Maximilian Braun (SAP) <maximilian.braun@sap.com>
2026-03-18 01:38:00 +0100

    chore: remove unused Docusaurus default assets
    - Remove undraw_*.svg, docusaurus.png, logo.svg, docusaurus-social-card.jpg
    - Update social card image to co_axolotl.png
    
    Signed-off-by: Maximilian Braun (SAP) <maximilian.braun@sap.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/co_axolotl.png',
     navbar: {
       title: 'Open Control Plane',
       logo: {
